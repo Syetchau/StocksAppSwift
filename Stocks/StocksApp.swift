af9c6e76be2d89f928cad862d9b69fbeb29e833d
syetchau95 <syetchau@hotmail.com>
2022-03-19 16:05:39 +0800

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by LiewSyetChau on 19/3/22.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
